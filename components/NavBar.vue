deb65ea9d099da534e3862ad1222fca1bc8e33aa
Rezig Rachid <rachid.rezig@ynov.com>
2024-12-03 16:50:09 +0100

    Creation du composant navBar (non responsie no animation)

---
+++ components/NavBar.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="contentNav">
+    <nav>
+      <div class="NavBtn">
+          <div class="ButtonNav">
+            <a class="TextNav">Acceuil</a>
+          </div>
+
+          <div class="ButtonNav">
+            <a class="TextNav">À propos</a>
+          </div>
+
+          <div class="ButtonNav">
+            <a class="TextNav">Évènement</a>
+          </div>
+
+          <div class="ButtonNav">
+            <a class="TextNav">Article</a>
+          </div>
+
+          <div class="ButtonNav">
+            <a class="TextNav">Contact</a>
+          </div>
+
+          <div class="ButtonNav">
+            <a class="TextNav">Connexion</a>
+          </div>
+      </div>
+      <div class="ConteneurImage">
+        <div class="ButtonImg">
+            <svg width="20" height="20" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="path-1-inside-1_200_63" fill="white">
+            <path d="M24.0465 21.9252L30.4706 28.3493L28.3493 30.4706L21.9252 24.0465C19.6154 25.8945 16.686 27 13.5 27C6.048 27 0 20.952 0 13.5C0 6.048 6.048 0 13.5 0C20.952 0 27 6.048 27 13.5C27 16.686 25.8945 19.6154 24.0465 21.9252ZM21.0371 20.8122C22.8713 18.9219 24 16.3434 24 13.5C24 7.69875 19.3013 3 13.5 3C7.69875 3 3 7.69875 3 13.5C3 19.3013 7.69875 24 13.5 24C16.3434 24 18.9219 22.8713 20.8122 21.0371L21.0371 20.8122Z"/>
+            </mask>
+            <path d="M24.0465 21.9252L30.4706 28.3493L28.3493 30.4706L21.9252 24.0465C19.6154 25.8945 16.686 27 13.5 27C6.048 27 0 20.952 0 13.5C0 6.048 6.048 0 13.5 0C20.952 0 27 6.048 27 13.5C27 16.686 25.8945 19.6154 24.0465 21.9252ZM21.0371 20.8122C22.8713 18.9219 24 16.3434 24 13.5C24 7.69875 19.3013 3 13.5 3C7.69875 3 3 7.69875 3 13.5C3 19.3013 7.69875 24 13.5 24C16.3434 24 18.9219 22.8713 20.8122 21.0371L21.0371 20.8122Z" fill="#2F2F2F"/>
+            <path d="M24.0465 21.9252L-0.159818 2.55889L-17.4941 24.2253L2.12619 43.8455L24.0465 21.9252ZM30.4706 28.3493L52.3909 50.2696L74.3112 28.3493L52.3909 6.42894L30.4706 28.3493ZM28.3493 30.4706L6.42894 52.3909L28.3493 74.3112L50.2696 52.3909L28.3493 30.4706ZM21.9252 24.0465L43.8455 2.12619L24.2253 -17.4941L2.55889 -0.159818L21.9252 24.0465ZM21.0371 20.8122L42.9574 42.7325L43.1224 42.5674L43.285 42.3999L21.0371 20.8122ZM20.8122 21.0371L42.3999 43.285L42.5674 43.1224L42.7325 42.9574L20.8122 21.0371ZM2.12619 43.8455L8.55024 50.2696L52.3909 6.42894L45.9668 0.0048912L2.12619 43.8455ZM8.55024 6.42894L6.42894 8.55024L50.2696 52.3909L52.3909 50.2696L8.55024 6.42894ZM50.2696 8.55024L43.8455 2.12619L0.0048912 45.9668L6.42894 52.3909L50.2696 8.55024ZM2.55889 -0.159818C5.54117 -2.5458 9.3887 -4 13.5 -4V58C23.9833 58 33.6895 54.3348 41.2915 48.2528L2.55889 -0.159818ZM13.5 -4C23.1688 -4 31 3.83117 31 13.5H-31C-31 38.0728 -11.0728 58 13.5 58V-4ZM31 13.5C31 23.1688 23.1688 31 13.5 31V-31C-11.0728 -31 -31 -11.0728 -31 13.5H31ZM13.5 31C3.83117 31 -4 23.1688 -4 13.5H58C58 -11.0728 38.0728 -31 13.5 -31V31ZM-4 13.5C-4 9.3887 -2.5458 5.54117 -0.159818 2.55889L48.2528 41.2915C54.3348 33.6895 58 23.9833 58 13.5H-4ZM43.285 42.3999C50.5101 34.9539 55 24.7167 55 13.5H-7C-7 7.97012 -4.76756 2.88994 -1.21091 -0.77549L43.285 42.3999ZM55 13.5C55 -9.42208 36.4221 -28 13.5 -28V34C2.18042 34 -7 24.8196 -7 13.5H55ZM13.5 -28C-9.42208 -28 -28 -9.42208 -28 13.5H34C34 24.8196 24.8196 34 13.5 34V-28ZM-28 13.5C-28 36.4221 -9.42208 55 13.5 55V-7C24.8196 -7 34 2.18042 34 13.5H-28ZM13.5 55C24.7167 55 34.9539 50.5101 42.3999 43.285L-0.77549 -1.21091C2.88994 -4.76756 7.97012 -7 13.5 -7V55ZM42.7325 42.9574L42.9574 42.7325L-0.88326 -1.10811L-1.10811 -0.88326L42.7325 42.9574Z" fill="#2F2F2F" mask="url(#path-1-inside-1_200_63)"/>
+          </svg>
+        </div>  
+
+        <div class="ButtonImg">
+          <svg width="35" height="35" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_b_124_194)">
+            <path d="M13.957 45.2207C13.957 37.4887 20.3743 31.2207 28.2904 31.2207C36.2065 31.2207 42.6237 37.4887 42.6237 45.2207H39.0404C39.0404 39.4217 34.2274 34.7207 28.2904 34.7207C22.3533 34.7207 17.5404 39.4217 17.5404 45.2207H13.957ZM28.2904 29.4707C22.351 29.4707 17.5404 24.772 17.5404 18.9707C17.5404 13.1695 22.351 8.4707 28.2904 8.4707C34.2297 8.4707 39.0404 13.1695 39.0404 18.9707C39.0404 24.772 34.2297 29.4707 28.2904 29.4707ZM28.2904 25.9707C32.2499 25.9707 35.457 22.8382 35.457 18.9707C35.457 15.1032 32.2499 11.9707 28.2904 11.9707C24.3308 11.9707 21.1237 15.1032 21.1237 18.9707C21.1237 22.8382 24.3308 25.9707 28.2904 25.9707Z" fill="#2F2F2F"/>
+            </g>
+            <defs>
+            <filter id="filter0_b_124_194" x="-100" y="-100" width="257" height="257" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="50"/>
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_124_194"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_124_194" result="shape"/>
+            </filter>
+            </defs>
+          </svg>
+
+        </div>
+      </div>  
+  </nav>
+ 
+
+  </div>
+  
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+</script>
+<style scoped>
+
+nav{
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  background-color: white;
+   border:  1px solid black;
+   border-radius: 36px;
+  box-shadow: 3px 3px 3px gray;
+}
+
+.NavBtn{
+ 
+  display: flex;
+   height: 51px;
+   width: 987;
+   justify-content: center;
+   align-items: center;
+   flex-direction: row; 
+   
+}
+
+.contentNav{
+  display: flex;
+  justify-content: center;
+  flex-direction: row; 
+ margin-top: 65px;
+
+   align-items: center;
+}
+
+.ButtonNav{
+width: 125px;
+
+top: 81px;
+left: 88px;
+gap: 0px;
+opacity: 0px;
+align-items: center;
+text-align: center;
+
+}
+
+.ButtonImg{
+
+opacity: 0px;
+align-items: center;
+text-align: center;
+
+}
+
+.TextNav{
+font-family: 'Druk Text Wide Trial', sans-serif;; /* Druk Text Wide Trial*/  
+font-size: 20px;
+font-weight: 700;
+line-height: 120px;
+text-align: center;
+text-decoration-skip-ink: none; 
+color: black
+
+
+
+
+}
+
+.ButtonNav :hover{
+width: 125px;
+
+top: 81px;
+left: 88px;
+gap: 0px;
+opacity: 0px;
+font-size: 18px;
+text-align: center;
+
+}
+
+.ConteneurImage{
+  margin-left: 100px;
+  margin-right: 30px;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 15px;
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+import NavBar from '../components/NavBar.vue'
+</script>
+
+
+
+
 <template>
+
+<NavBar>
+
+</NavBar>
+
   This is the home page
 </template>
+
