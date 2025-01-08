c7f767d8130dae38908815f621729050d0bbda02
Rezig Rachid <rachid.rezig@ynov.com>
2025-01-08 16:43:56 +0100

    Ajout page home

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import NavBar from '../components/NavBar.vue'
+import BodyHome from '~/components/BodyHome.vue';
 </script>
 
 
@@ -11,18 +12,39 @@ import NavBar from '../components/NavBar.vue'
   <NavBar>
 
 </NavBar>
+
+<BodyHome>
+  
+</BodyHome>
+
+
+  
+
 </body>
 
 
-  This is the home page
+ 
 </template>
 
 <style>
   @import url('https://fonts.cdnfonts.com/css/druk-text-wide-trial');
+  @import url(https://db.onlinewebfonts.com/c/3aea52b2b9b658705ec5162ab6d2b7e9?family=Stevie+Sans+W00+Regular);
 </style>
                 
 <style scoped>
+
+
+
+
+
 body{
   background-color: #F8FAEC;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 }
+
+
+
+
 </style>
