1ff72544f4be5f610f85f0c06259df23783a8799
crtnsj <crtnsj.pro@gmail.com>
2025-01-29 10:24:19 +0100

    Fix NavBar, the NavBar was in index.vue, it should be in app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+</script>
+
 <template>
   <div>
+    <NavBar />
     <NuxtPage />
-    <Footer/>
+    <Footer />
   </div>
 </template>
+
+<style>
+@import url('https://fonts.cdnfonts.com/css/druk-text-wide-trial');
+</style>
+
+<style scoped>
+body {
+  background-color: #f8faec;
+}
+</style>
