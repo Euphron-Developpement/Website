47324a1be3b7b35b2fbbd3dc5bf61efc956c3dc1
philippe-p <134625964+Philippe-SP@users.noreply.github.com>
2025-01-29 16:48:11 +0100

    Ajout du bouton reservation

--- pages/event.vue
+++ pages/event.vue
@@ -9,6 +9,7 @@
         <div class="event_content">
             <div class="event_image">
                 <img src="https://s3-alpha-sig.figma.com/img/4acb/d005/4d4426fe0f6db745f32b35393cd7840c?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pd2y5262JQaWxGi3av5XnIHp4I-8wpqkbLXm2uVh93aETMff~OQwvpIbevuhOdX8bzZXuwLU9VMTgSOqNhQtROBAazOoNRKZ1CB5V6zifbXPKAVNUXHU9JsIjkP8tfL3Pv1h9LcUg6hBuCZjkqpLZSrtw0ZYX-6rBmJ3lIAFgc4o2IAvzeRgzDe7hGOZyWBY0xhLAPRk0ePeX9mNO0TZP76080zdnGff1HNXYb2syz4yuqdMcyDJaGad6x8oDpAwdr7A3Oyi6WJ~ID0~65kpm8GtEPVpgyY1NubxnsXFs7zTLfwm6O2RGC0NBiOrM-FtjG75-UXicOcXbGL0gKy3lg__">
+                <button class="reservation_btn">Réservez votre place dès maintenant</button>
             </div>
             <div class="event_text">
                 <p>
@@ -67,12 +68,28 @@
 
 .event_image {
     width: 100%;
+    position: relative;
 }
 
 .event_image img {
     width: 100%;
     height: auto;
     aspect-ratio: auto;
+}
+
+.reservation_btn {
+    width: 140px;
+    height: 140px;
+    background-color: #be2525b1;
+    color: #F8FAEC;
+    padding: 10px;
+    border: none;
+    border-radius: 50%;
+    text-wrap: wrap;
+    text-align: center;
+    position: absolute;
+    bottom: 15px;
+    left: 15px; 
 }
 
 .event_text {
@@ -89,11 +106,25 @@
     }
 }
 
+@media screen and (max-width: 920px) {
+    .reservation_btn {
+        width: 100px;
+        height: 100px;
+        font-size: 12px;
+    }
+}
+
 /* Mobile */
 @media screen and (max-width: 700px) {
     .event_content {
         display: flex;
         flex-direction: column;
+    }
+
+    .reservation_btn {
+        width: 120px;
+        height: 120px;
+        font-size: 16px;
     }
 }
 </style>
