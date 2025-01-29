ed37376f343aa2a7d0d88975d068aed5d5c3dd33
Tokennn <quentin.contreau@outlook.fr>
2025-01-29 14:31:50 +0100

    feat : bugs email + mdp terminal fix

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,19 @@
 <script setup>
 import "~/assets/css/login.css";
+
+const email = ref('');
+const password = ref('');
+const handleSubmit = () => {
+  console.log('Email:', email.value);
+  console.log('Password:', password.value);
+};
+
+
 </script>
 
 <template>
   <div class="form-container">
     <div class="header-container">
-        <button @click="goBack" class="back-button">‹</button>
         <h1>Se Connecter</h1>
     </div>
     <form @submit.prevent="handleSubmit">
@@ -13,12 +21,12 @@ import "~/assets/css/login.css";
 
       <div class="form-group">
         <label for="email">Adresse mail</label>
-        <input v-model="email" type="email" id="email"  />
+        <input v-model="email" type="email" id="email"  placeholder=""/>
       </div>
 
       <div class="form-group">
         <label for="password">Mot de passe</label>
-        <input v-model="password" type="password" id="password" />
+        <input v-model="password" type="password" id="password" placeholder=""/>
       </div>
 
       <button type="submit" class="submit-btn">Se Connecter</button>
