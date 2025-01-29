50456c66c4842d5b0ba248a316cc595904773872
quentinmel <qmeleo@gmail.com>
2025-01-29 14:10:13 +0100

    ft: registration page

---
+++ pages/inscription.vue
@@ -0,0 +1,45 @@
+<script setup>
+import "~/assets/css/inscription.css";
+</script>
+
+<template>
+    <div class="form-container">
+        <h1>CREER UN COMPTE</h1>
+        <form @submit.prevent="handleSubmit" id="registration-form">
+            <div class="form-group">
+                <label for="prenom">Prénom</label>
+                <input v-model="prenom" type="text" id="prenom"
+                    placeholder="______________________________________________________________" />
+            </div>
+
+            <div class="form-group">
+                <label for="nom">Nom</label>
+                <input v-model="nom" type="text" id="nom"
+                    placeholder="______________________________________________________________" />
+            </div>
+
+            <div class="form-group">
+                <label for="email">Adresse mail</label>
+                <input v-model="email" type="email" id="email"
+                    placeholder="______________________________________________________________" />
+            </div>
+
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input v-model="password" type="password" id="password"
+                    placeholder="______________________________________________________________" />
+            </div>
+
+            <div class="form-group">
+                <label for="password">Confirmation de mot de passe</label>
+                <input v-model="password" type="password" id="confirm-password"
+                    placeholder="______________________________________________________________" />
+            </div>
+
+            <button type="submit" class="submit-btn">S'inscrire</button>
+            <p class="already-account">
+                <a href="#">J'ai déjà un compte</a>
+            </p>
+        </form>
+    </div>
+</template>
