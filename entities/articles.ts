a42a57691918e737b1a65d654f7e804aaabaf0c3
crtnsj <crtnsj.pro@gmail.com>
2025-04-02 16:52:39 +0200

    Add Article interface and API route for fetching articles

---
+++ entities/articles.ts
@@ -0,0 +1,10 @@
+export interface Article {
+  id: number;
+  title: string;
+  read_time: number;
+  publication_date: string;
+  content: string;
+  interview_time: number;
+  author: string;
+  category: string;
+}
