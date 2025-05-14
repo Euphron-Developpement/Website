b7a714ff552b9227102210a221a01942ba55b0c8
quentinmel <112957970+quentinmel@users.noreply.github.com>
2025-05-14 09:02:11 +0200

    feature: redid the entire navbar (#26)

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,55 @@
 <template>
   <div class="contentNav">
     <nav>
+      <!-- Icônes des médias sociaux - cachées par défaut, visibles uniquement en dessous de 1250px -->
+      <div class="socialIcons">
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.8334 11.0833C13.2092 11.0833 11.0833 13.2099 11.0833 15.8333C11.0833 18.4574 13.2099 20.5833 15.8334 20.5833C18.4574 20.5833 20.5834 18.4568 20.5834 15.8333C20.5834 13.2093 18.4568 11.0833 15.8334 11.0833ZM15.8334 7.91667C20.204 7.91667 23.75 11.4587 23.75 15.8333C23.75 20.2041 20.208 23.75 15.8334 23.75C11.4626 23.75 7.91667 20.2079 7.91667 15.8333C7.91667 11.4626 11.4587 7.91667 15.8334 7.91667ZM26.125 7.51949C26.125 8.61216 25.2371 9.49868 24.1459 9.49868C23.0532 9.49868 22.1667 8.6108 22.1667 7.51949C22.1667 6.42817 23.0545 5.54167 24.1459 5.54167C25.2357 5.54029 26.125 6.42817 26.125 7.51949ZM15.8334 3.16667C11.9154 3.16667 11.2768 3.17704 9.45454 3.25818C8.21302 3.31647 7.38079 3.48344 6.60754 3.78364C5.92037 4.05015 5.42467 4.36838 4.89652 4.89654C4.36634 5.42672 4.04868 5.92108 3.78325 6.60856C3.48235 7.38359 3.31542 8.21451 3.25818 9.45432C3.17625 11.2024 3.16667 11.8133 3.16667 15.8333C3.16667 19.7513 3.17704 20.3899 3.25817 22.212C3.31648 23.453 3.48368 24.2864 3.78315 25.0578C4.05036 25.7459 4.36924 26.2428 4.89484 26.7685C5.42711 27.3 5.92295 27.6187 6.60391 27.8815C7.38658 28.1841 8.21829 28.3513 9.4543 28.4085C11.2024 28.4903 11.8133 28.5 15.8334 28.5C19.7513 28.5 20.3899 28.4895 22.212 28.4085C23.4503 28.3504 24.2844 28.1827 25.0579 27.8834C25.7441 27.617 26.2425 27.297 26.7685 26.7718C27.3008 26.2387 27.6189 25.744 27.8821 25.0615C28.1838 24.2817 28.3514 23.4488 28.4085 22.2124C28.4904 20.4643 28.5 19.8533 28.5 15.8333C28.5 11.9154 28.4896 11.2768 28.4085 9.45467C28.3502 8.21601 28.1826 7.38069 27.883 6.60753C27.6172 5.92227 27.2978 5.42505 26.7701 4.89654C26.239 4.36549 25.746 4.04847 25.058 3.78325C24.2836 3.4826 23.4514 3.31544 22.2123 3.2582C20.4643 3.17625 19.8533 3.16667 15.8334 3.16667ZM15.8334 0C20.1347 0 20.6717 0.0158332 22.3607 0.0949999C24.0455 0.172852 25.1948 0.439375 26.2042 0.83125C27.2478 1.23369 28.1292 1.77729 29.0092 2.65736C29.888 3.53744 30.4317 4.42146 30.8354 5.4625C31.2259 6.47056 31.4925 7.62111 31.5717 9.30604C31.6469 10.9949 31.6667 11.5319 31.6667 15.8333C31.6667 20.1348 31.6509 20.6717 31.5717 22.3606C31.4938 24.0456 31.2259 25.1948 30.8354 26.2042C30.433 27.2479 29.888 28.1292 29.0092 29.0094C28.1292 29.8881 27.2438 30.4317 26.2042 30.8354C25.1948 31.226 24.0455 31.4925 22.3607 31.5717C20.6717 31.6469 20.1347 31.6667 15.8334 31.6667C11.5319 31.6667 10.9949 31.6508 9.30604 31.5717C7.62111 31.4938 6.47319 31.226 5.4625 30.8354C4.42013 30.4329 3.53742 29.8881 2.65736 29.0094C1.77729 28.1292 1.235 27.2439 0.83125 26.2042C0.439375 25.1948 0.174167 24.0456 0.0950003 22.3606C0.019792 20.6717 0 20.1348 0 15.8333C0 11.5319 0.0158336 10.9949 0.0950003 9.30604C0.172837 7.61979 0.439375 6.47187 0.83125 5.4625C1.23367 4.42014 1.77729 3.53744 2.65736 2.65736C3.53742 1.77729 4.42146 1.235 5.4625 0.83125C6.47188 0.439375 7.61979 0.174167 9.30604 0.0949999C10.9949 0.0197916 11.5319 0 15.8334 0Z"
+              fill="#2F2F2F"
+            />
+          </svg>
+          <svg
+            width="24"
+            height="28"
+            viewBox="0 0 24 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17.3333 8.82716V18.5C17.3333 23.2865 13.4532 27.1667 8.66667 27.1667C3.8802 27.1667 0 23.2865 0 18.5C0 13.7135 3.8802 9.83333 8.66667 9.83333C9.35507 9.83333 10.0247 9.91359 10.6667 10.0652V14.2824C10.0605 13.9944 9.3824 13.8333 8.66667 13.8333C6.08933 13.8333 4 15.9227 4 18.5C4 21.0773 6.08933 23.1667 8.66667 23.1667C11.244 23.1667 13.3333 21.0773 13.3333 18.5V0.5H17.3333C17.3333 4.18189 20.3181 7.16667 24 7.16667V11.1667C21.4775 11.1667 19.1595 10.291 17.3333 8.82716Z"
+              fill="#2F2F2F"
+            />
+          </svg>
+          <svg
+            width="32"
+            height="26"
+            viewBox="0 0 32 26"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M27.876 4.90781C27.7554 4.43619 27.4142 4.08035 27.0234 3.97058C26.3291 3.777 22.9584 3.33268 15.8334 3.33268C8.70833 3.33268 5.33827 3.77717 4.64118 3.97113C4.25407 4.0788 3.91292 4.43457 3.79069 4.90781C3.61842 5.5798 3.16667 8.39294 3.16667 12.8327C3.16667 17.2723 3.61842 20.0856 3.79117 20.7595C3.91128 21.2293 4.25249 21.585 4.64159 21.6943C5.33827 21.8882 8.70833 22.3327 15.8334 22.3327C22.9584 22.3327 26.3291 21.8884 27.0255 21.6943C27.4126 21.5866 27.7538 21.2308 27.876 20.7576C28.0483 20.0856 28.5 17.266 28.5 12.8327C28.5 8.39935 28.0483 5.5798 27.876 4.90781ZM30.9434 4.12147C31.6667 6.94268 31.6667 12.8327 31.6667 12.8327C31.6667 12.8327 31.6667 18.7227 30.9434 21.5439C30.5405 23.1042 29.3645 24.3305 27.874 24.745C25.1689 25.4993 15.8334 25.4993 15.8334 25.4993C15.8334 25.4993 6.50337 25.4993 3.7927 24.745C2.29659 24.3248 1.1207 23.0984 0.723204 21.5439C2.83122e-07 18.7227 0 12.8327 0 12.8327C0 12.8327 2.83122e-07 6.94268 0.723204 4.12147C1.12621 2.56117 2.30212 1.3348 3.7927 0.920252C6.50337 0.166015 15.8334 0.166016 15.8334 0.166016C15.8334 0.166016 25.1689 0.166015 27.874 0.920252C29.3701 1.34056 30.546 2.56692 30.9434 4.12147ZM12.6667 18.3743V7.29102L22.1667 12.8327L12.6667 18.3743Z"
+              fill="#2F2F2F"
+            />
+          </svg>
+      </div>
+      
       <div :class="{ NavBtn: true, active: navOpen }">
-        <div class="close_nav" @click="toggleNav()">Fermer la navBar</div>
+        <div class="close_nav" @click="toggleNav()">
+          <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L17 17M1 17L17 1" stroke="#2F2F2F" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </div>
         <div class="ButtonNav">
           <a href="/" :class="{ TextNav: true, active: isActive('/') }"
-          >Accueil</a
+            >Accueil</a
           >
         </div>
 
@@ -13,7 +57,7 @@
           <a
             href="/propos"
             :class="{ TextNav: true, active: isActive('/propos') }"
-          >A propos</a
+            >A propos</a
           >
         </div>
 
@@ -21,7 +65,7 @@
           <a
             href="/event"
             :class="{ TextNav: true, active: isActive('/event') }"
-          >Evenement</a
+            >Evenement</a
           >
         </div>
 
@@ -29,7 +73,7 @@
           <a
             href="/article"
             :class="{ TextNav: true, active: isActive('/article') }"
-          >Article</a
+            >Article</a
           >
         </div>
 
@@ -37,15 +81,23 @@
           <a
             href="/contact"
             :class="{ TextNav: true, active: isActive('/contact') }"
-          >Contact</a
+            >Contact</a
+          >
+        </div>
+
+        <div class="ButtonNav mobile-only-account">
+          <a
+            href="/account"
+            :class="{ TextNav: true, active: isActive('/account') }"
+            >Mon compte</a
           >
         </div>
       </div>
       <div class="ConteneurImage">
-        <a href="/login" class="ButtonImg">
+        <a href="/account" class="ButtonImg">
           <svg
-            width="35"
-            height="35"
+            width="45"
+            height="45"
             viewBox="0 0 57 57"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -90,7 +142,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
@@ -104,6 +156,14 @@ const toggleNav = () => {
   navOpen.value = !navOpen.value;
   document.body.style.overflow = navOpen.value ? 'hidden' : 'auto';
 };
+
+onBeforeUnmount(() => {
+  document.body.style.overflow = 'auto';
+});
+
+onMounted(() => {
+  document.documentElement.style.overflowX = 'hidden';
+});
 </script>
 
 <style scoped>
@@ -116,7 +176,8 @@ nav {
   border-radius: 999px;
   box-shadow: 2px 2px 2px #00000040;
   min-height: 4.5rem;
-  width: 70vw;
+  width: 90vw;
+  height: 4.5rem;
 }
 
 .NavBtn {
@@ -126,7 +187,7 @@ nav {
   width: 987px;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
+  gap: 3rem;
   transition: all 0.3s ease-in-out;
 }
 
@@ -152,12 +213,14 @@ nav {
 
 .TextNav {
   font-family: 'Druk Text Wide Trial', sans-serif;
-  font-size: 20px;
   font-weight: 700;
-  line-height: 120px;
+  font-size: 20px;
   text-align: center;
   text-decoration: none;
-  color: #2f2f2f;
+}
+
+.TextNav.active {
+    color: #be2625;
 }
 
 .ButtonNav :hover {
@@ -171,8 +234,10 @@ nav {
   align-items: center;
 }
 
-.active {
-  color: #be2625;
+.socialIcons {
+  display: none;
+  gap: 25px;
+  margin-left: 25px;
 }
 
 .burger_nav {
@@ -186,29 +251,49 @@ nav {
 
 .close_nav {
   display: none;
-  background-color: #f8faec;
+  background-color: transparent;
   color: #2f2f2f;
-  padding: 4px;
+  padding: 8px;
   cursor: pointer;
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  border: none;
+  z-index: 1000;
 }
 
-@media (max-width: 1180px) {
-  .NavBtn {
-    gap: 0;
+.mobile-only-account {
+  display: none;
+}
+
+@media (max-width: 1175px) {
+  nav {
+    width: 90vw;
   }
 
   .TextNav {
-    font-size: 16px;
+    font-size: 15px;
   }
 }
 
-@media (max-width: 910px) {
-  nav {
-    width: 80vw;
+@media (max-width: 975px) {
+  .TextNav {
+    font-size: 1em;
   }
 
-  .ButtonNav {
-    margin-left: 13px;
+  .ButtonImg svg{
+    width: 40px;
+    height: 40px;
+  }
+}
+
+@media (max-width: 975px) {
+  .NavBtn {
+    gap: 1rem;
+  }
+
+  .TextNav {
+    font-size: 0.8em;
   }
 }
 
@@ -218,42 +303,113 @@ nav {
   }
 
   nav {
-    width: 93vw;
+    width: 90vw;
     box-shadow: none;
-    justify-content: end;
+    justify-content: space-between;
   }
 
   .close_nav {
-    display: block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #f8faec;
   }
 
   .NavBtn {
-    position: absolute;
+    position: fixed;
+    gap: 0;
     height: 100vh;
     right: 0;
     top: 0;
     width: 90vw;
-    background-color: #2f2f2f;
+    background-color: #f8faec;
     flex-direction: column;
     transform: translateX(100%);
     z-index: 999;
+    visibility: hidden;
+    padding-top: 60px;
+    align-items: flex-start;
+    justify-content: flex-start;
   }
 
   .NavBtn.active {
     transform: translateX(0);
+    visibility: visible;
+    box-shadow: -5px 0 15px rgba(0, 0, 0, 0.1);
   }
 
   .TextNav {
-    color: #f8faec;
+    color: #2f2f2f;
+    font-size: 24px;
+    line-height: 70px;
+    text-align: left;
+  }
+
+  .ButtonNav {
+    margin: 5px 0;
+    width: 100%;
+    text-align: left;
+    padding-left: 40px;
+    margin-left: 13px;
   }
 
   .burger_nav {
     display: block;
+    width: 40px;
+    cursor: pointer;
   }
 
   .ConteneurImage {
     margin: 0;
   }
+
+  .ButtonImg svg{
+    margin-right: 10px;
+    width: 50px;
+    height: 50px;
+  }
+  
+  .socialIcons {
+    display: flex;
+    margin-left: 15px;
+    order: -1;
+  }
+  
+  .mobile-only-account {
+    display: block;
+  }
+  
+  .desktop-only-account {
+    display: none;
+  }
 }
 
+@media (max-width: 450px) {
+  .ConteneurImage {
+    margin-right: 0;
+  }
+
+  .ButtonImg svg {
+    margin-right: 0;
+    width: 40px;
+    height: 40px;
+  }
+
+  .burger_nav {
+    width: 35px;
+    height: 35px;
+  }
+
+  .socialIcons {
+    gap: 15px;
+  }
+
+  .socialIcons svg {
+    width: 15px;
+    height: 15px;
+  }
+}
 </style>
